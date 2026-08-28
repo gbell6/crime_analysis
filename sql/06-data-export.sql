77697660a8bb56be55d4ee22463a758371f2b36d
Garrett Bell <gt.bell20@gmail.com>
2026-08-27 21:50:20 -0500

    create sql script that will export the needed data to csv files within project directory; viz_data/... is the resulting output.

--- sql/06-data-export.sql
+++ sql/06-data-export.sql
@@ -1,6 +1,6 @@
 -- Create client-side export of data to be visualized since Tableau Public cannot connect to local db servers
-\copy county_common_crimes to '../viz\ data/' with (format csv, header, delimiter ',')
-\copy crimes_by_race to '../viz\ data/' with (format csv, header, delimiter ',')
-\copy nibrs_month to '../viz\ data/' with (format csv, header, delimiter ',')
-\copy nibrs_offender to '../viz\ data/' with (format csv, header, delimiter ',')
--- Execute via psql cli with "psql -d portfolio_proj -f 'sql/06-data-export.sql'"
+\copy (select * from county_common_crimes) to 'viz_data/county_common_crimes.csv' with (format csv, header)
+\copy (select * from crimes_by_race) to 'viz_data/crimes_by_race.csv' with (format csv, header)
+\copy nibrs_month to 'viz_data/month.csv' with (format csv, header)
+\copy nibrs_offender to 'viz_data/offender.csv' with (format csv, header)
+-- Execute via psql cli with "\i sql/06-data-export.sql"
