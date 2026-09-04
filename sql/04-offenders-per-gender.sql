e47ffa1b8cac83e7c48f08667473350e731920d1
Garrett Bell <gt.bell20@gmail.com>
2026-09-04 18:24:33 -0500

    created view for offenses_per_gender; added offender_gender data to data export for visualization

--- sql/04-offenders-per-gender.sql
+++ sql/04-offenders-per-gender.sql
@@ -1,6 +1,8 @@
 -- Find out how many crimes each gender committed.
+create or replace view offender_gender as (
 select
 	count(*) filter (where sex_code = 'M') as male_offenders,
 	count(*) filter (where sex_code = 'F') as female_offenders,
 	count(*) filter (where sex_code != 'F' and sex_code != 'M') as unknown_gender
-from nibrs_offender;
+from nibrs_offender
+);
--- sql/06-data-export.sql
+++ sql/06-data-export.sql
@@ -1,6 +1,7 @@
 -- Create client-side export of data to be visualized since Tableau Public cannot connect to local db servers
 \copy (select * from county_common_crimes) to 'viz_data/county_common_crimes.csv' with (format csv, header)
 \copy (select * from crimes_by_race) to 'viz_data/crimes_by_race.csv' with (format csv, header)
+\copy (select * from offender_gender) to 'viz_data/offender_gender.csv' with (format csv, header)
 \copy nibrs_month to 'viz_data/month.csv' with (format csv, header)
 \copy nibrs_offender to 'viz_data/offender.csv' with (format csv, header)
 -- Execute via psql cli with "\i sql/06-data-export.sql"
